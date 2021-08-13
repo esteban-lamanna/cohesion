98211866a7f79bf8bc08feb7e5b4baf850d908ae
esteban lamanna <estebanlamanna@hotmail.com>
2021-08-13 12:33:50 -0300

    first test

--- Tests/ServiceRequestTest.cs
+++ Tests/ServiceRequestTest.cs
@@ -1,16 +1,50 @@
+using CohesionTest.Configuration;
+using CohesionTest.Models.Entities;
+using CohesionTest.Repository;
+using CohesionTest.Repository.TestData;
+using CohesionTest.Services;
+using Moq;
 using NUnit.Framework;
+using System;
+using System.Threading.Tasks;
 
 namespace UnitTests
 {
     public class ServiceRequestTest
     {
+        private Mock<IServiceRequestRepository> _serviceRequestRepository;
+        private Mock<IUserRepository> _userRepository;
+        private Mock<IServiceProvider> _serviceProvider;
+
         [SetUp]
         public void Setup()
         {
+            _serviceRequestRepository = new Mock<IServiceRequestRepository>();
+            _userRepository = new Mock<IUserRepository>();
+            _serviceProvider = new Mock<IServiceProvider>();
         }
 
         [Test]
-        public void CreationResponseStatusCodeIsOK()
+        public async Task CreateServiceRequest()
+        {
+            var serviceRequestService = new ServiceRequestService(_serviceRequestRepository.Object,
+                                                                  _userRepository.Object,
+                                                                  _serviceProvider.Object,
+                                                                  new ApplicationOptions());
+
+            var serviceRequest = ServiceRequest.CreateInstance();
+            serviceRequest.Building = MockData.EmpireState;
+            serviceRequest.User = MockData.UserJohn;
+            serviceRequest.Description = "test";
+            serviceRequest.SetStatus(ServiceRequest.PossibleStates.Created, serviceRequest.User);
+
+            await serviceRequestService.CreateAsync(serviceRequest);
+
+            Assert.IsTrue(true);
+        }
+
+        [Test]
+        public void IfStatusIsCancelledSendNotification()
         {
 
 

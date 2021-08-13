5a2b880e54c00356296885edbca2b28c39ba2f70
esteban lamanna <estebanlamanna@hotmail.com>
2021-08-13 09:58:33 -0300

    tests

---
+++ Tests/ServiceRequestTest.cs
@@ -0,0 +1,21 @@
+using NUnit.Framework;
+
+namespace UnitTests
+{
+    public class ServiceRequestTest
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void CreationResponseStatusCodeIsOK()
+        {
+
+
+
+
+        }
+    }
+}
